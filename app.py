c0c1c5b3c235d6658bca1d835415c79b649dc69a
Hubert Ryan <hubert.ryan@nickelpay.com.br>
2024-03-06 22:10:42 -0300

    persisting users data blen

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
+import requests
 from flask import Flask, jsonify
-from configure_user_blend import load_users_data
+from configure_user_blend import load_users_data, users_blend_path
 from routes.user_routes import user_routes
 
 app = Flask(__name__)
@@ -11,5 +12,22 @@ def index():
     all_users = load_users_data()
     return jsonify({'message': 'Nice! You\'re using Oddity database.', 'total_users': len(all_users)})
 
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+def main():
+    res = requests.get("https://python-flask-e9bh.onrender.com/users/")
+    data_users = res.json()
+    data_old_users = load_users_data()
+
+    if(len(data_users) > len(data_old_users)):
+         with open(users_blend_path, 'w') as json_file:
+            json.dump(data_users, json_file, indent=4)
+
+    print('Success persisting data from users.\n')
+
+
+main()
