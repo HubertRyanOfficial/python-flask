c714099002b39f19187c7527b9af99a30563e9b0
Hubert Ryan <hubert.ryan@nickelpay.com.br>
2024-03-06 21:40:20 -0300

    now showing length of users collection

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask, jsonify
-
+from configure_user_blend import load_users_data
 from routes.user_routes import user_routes
 
 app = Flask(__name__)
@@ -8,7 +8,8 @@ app.register_blueprint(user_routes, url_prefix='/users')
 
 @app.route('/')
 def index():
-    return jsonify({'message': 'Nice! You\'re using Oddity database.'})
+    all_users = load_users_data()
+    return jsonify({'message': 'Nice! You\'re using Oddity database.', 'total_users': len(all_users)})
 
 if __name__ == "__main__":
     app.run(debug=True)
